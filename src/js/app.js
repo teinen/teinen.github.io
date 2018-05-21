212744902bfa26ced90886a15b127ff0ebf52d47
諦念 <teinen.github@gmail.com>
2018-05-21 22:06:03 +0900

    Change libraries local to CDN

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,14 @@
+// Vue.js
+import Vue from 'vue';
+import App from '../components/app.vue';
+
 // js modules
 import './modules/ityped';
 import { toggleTopNav } from './modules/toggleTopNav';
 
 // css modules
 import '../css/style.scss';
+
+window.onload = () => {
+  new Vue(App).$mount('#app');
+}
