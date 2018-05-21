586467c082ac269ae4f1eccf862b6c3002879645
諦念 <teinen.github@gmail.com>
2018-05-21 21:31:51 +0900

    Add Vue.js build config

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,75 @@
-const path = require("path")
-const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = [
   {
-    mode: "production",
-    entry: "./src/js/app.js",
+    mode: 'production',
+    entry: './src/js/app.js',
     output: {
-      filename: "bundle.js",
-      path: path.join(__dirname, "./public/")
+      filename: 'bundle.js',
+      path: path.join(__dirname, './public/')
     },
     module: {
       rules: [
         {
           test: /\.scss$/,
           loader: ExtractTextPlugin.extract({
-            fallback: "style-loader",
+            fallback: 'style-loader',
             use: [
               {
-                loader: "css-loader",
+                loader: 'css-loader',
                 options: { url: false }
               },
               {
-                loader: "postcss-loader",
+                loader: 'postcss-loader',
                 options: {
                   plugins: [
                     require('autoprefixer')({
-                      browsers: ["last 2 versions"]
+                      browsers: ['last 2 versions']
                     })
                   ]
                 }
               },
-              "sass-loader"]
+              'sass-loader']
           })
         },
         {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        },
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'vue-style-loader',
+            'css-loader'
+          ]
+        },
+        {
           test: /\.(png|jpeg|jpg)$/,
-          loader: "file-loader"
+          loader: 'file-loader'
         },
         {
           test: /\.(woff|eot|ttf|svg)$/,
-          loader: "file-loader"
+          loader: 'file-loader'
         }
       ]
     },
-    plugins: [
-      new ExtractTextPlugin("bundle.css"),
-    ],
     devServer: {
-      host: "0.0.0.0",
+      host: '0.0.0.0',
       port: 8080,
       disableHostCheck: true
-    }
+    },
+    plugins: [
+      new ExtractTextPlugin('bundle.css'),
+      new VueLoaderPlugin()
+    ],
   },
 ];
