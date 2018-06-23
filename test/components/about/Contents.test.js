4bb0b804a2e7d255e4a907196809f06f7ec5ebc3
諦念 <teinen.github@gmail.com>
2018-06-23 18:27:00 +0900

    Update test

--- test/components/about/Contents.test.js
+++ test/components/about/Contents.test.js
@@ -6,21 +6,21 @@ describe('about contents', () => {
   })
 
   test('has dafault data', () => {
-    const defaultData = Contents.data()
+    const items = Contents.data().items
 
-    expect(defaultData.items[0].id).toBe(1)
-    expect(defaultData.items[0].title).toBe('Name')
-    expect(defaultData.items[0].content).toBe('諦念(teinen)')
-    expect(defaultData.items[0].class).toBe('wow animated bounceInLeft')
+    expect(items[0].id).toBe(1)
+    expect(items[0].title).toBe('Name')
+    expect(items[0].content).toBe('諦念(teinen)')
+    expect(items[0].class).toBe('wow animated bounceInLeft')
 
-    expect(defaultData.items[1].id).toBe(2)
-    expect(defaultData.items[1].title).toBe('Job')
-    expect(defaultData.items[1].content).toBe('Server Side Engineer')
-    expect(defaultData.items[1].class).toBe('wow animated bounceInRight')
+    expect(items[1].id).toBe(2)
+    expect(items[1].title).toBe('Job')
+    expect(items[1].content).toBe('Server Side Engineer')
+    expect(items[1].class).toBe('wow animated bounceInRight')
 
-    expect(defaultData.items[2].id).toBe(3)
-    expect(defaultData.items[2].title).toBe('Hobby')
-    expect(defaultData.items[2].content).toBe('ゲーム, 漫画, アニメ, プログラミング')
-    expect(defaultData.items[2].class).toBe('wow animated bounceInLeft')
+    expect(items[2].id).toBe(3)
+    expect(items[2].title).toBe('Hobby')
+    expect(items[2].content).toBe('ゲーム, 漫画, アニメ, プログラミング')
+    expect(items[2].class).toBe('wow animated bounceInLeft')
   })
 })
