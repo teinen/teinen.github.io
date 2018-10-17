a72a15248d6eb29b46084c37fed8d965ec693511
Akira Kanno <teinen.github@gmail.com>
2018-10-17 22:53:57 +0900

    Add media query setting to Header

--- components/Header.vue
+++ components/Header.vue
@@ -20,16 +20,32 @@ header {
   background-color: #28385E;
   border-bottom: 10px solid #516C8D;
   display: flex;
-  height: 110px;
-  justify-content: start;
+  height: 100px;
+  justify-content: center;
   padding: 0 20px;
   position: fixed;
   top: 0;
   width: 100vw;
+
+  // For pc
+  @media screen and (min-width: 960px) {
+    justify-content: start;
+  }
 }
 
 .title {
+  // For mobile
   color: #ffffff;
-  font-size: 3.0em;
+  font-size: 1.5em;
+
+  // For tablet
+  @media screen and (min-width: 480px) and (max-width: 960px) {
+    font-size: 2.0em;
+  }
+
+  // For pc
+  @media screen and (min-width: 960px) {
+    font-size: 2.5em;
+  }
 }
 </style>
