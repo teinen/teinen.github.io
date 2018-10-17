7160493c32c538d7a249963642e0929277fcf026
Akira Kanno <teinen.github@gmail.com>
2018-10-17 23:55:48 +0900

    Add Header nav

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,15 @@
 <template>
   <header>
     <span class="title">{{ title }}</span>
+
+    <nav>
+      <ul>
+        <li><router-link to="#">ABOUT</router-link></li>
+        <li><router-link to="#">SKILL</router-link></li>
+        <li><router-link to="#">CAREER</router-link></li>
+        <li><router-link to="#">WORK</router-link></li>
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -18,34 +27,59 @@ export default {
 header {
   align-items: center;
   background-color: #28385E;
-  border-bottom: 10px solid #516C8D;
+  border-bottom: 8px solid #516C8D;
   display: flex;
-  height: 100px;
+  height: 80px;
   justify-content: center;
-  padding: 0 20px;
+  padding: 0 60px;
   position: fixed;
   top: 0;
   width: 100vw;
 
-  // For pc
+  @media screen and (min-width: 480px) {
+    border-bottom: 10px solid #516C8D;
+    height: 100px;
+  }
+
   @media screen and (min-width: 960px) {
     justify-content: start;
   }
 }
 
 .title {
-  // For mobile
   color: #ffffff;
   font-size: 1.5em;
 
-  // For tablet
   @media screen and (min-width: 480px) and (max-width: 960px) {
     font-size: 2.0em;
   }
 
-  // For pc
   @media screen and (min-width: 960px) {
     font-size: 2.5em;
+  }
+}
+
+nav {
+  font-size: 1.5em;
+  position: absolute;
+  right: 60px;
+
+  ul {
+    align-items: flex-end;
+    display: flex;
+    justify-content: space-between;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+
+    li {
+      margin: 0 0 0 20px;
+
+      a {
+        color: #ffffff;
+        text-decoration: none;
+      }
+    }
   }
 }
 </style>
