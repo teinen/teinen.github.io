b6e3301ee546352d425358834eecbd96e1952632
Akira Kanno <teinen.github@gmail.com>
2018-10-21 20:28:29 +0900

    Add each pages

--- pages/index.vue
+++ pages/index.vue
@@ -13,16 +13,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-section {
-  height: 100%;
-  margin: 120px 0;
-}
 
-.card {
-  background: #fff;
-  border-radius: 5px;
-  box-shadow: 0 2px 5px #ccc;
-  height: calc(100vh - 240px);
-  padding: 20px;
-}
 </style>
---
+++ pages/work.vue
@@ -0,0 +1,17 @@
+<template>
+  <section id="work">
+    <div class="card">
+      
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
