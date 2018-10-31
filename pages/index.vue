df9b84df087314d9cab760822e48451c0bfa6994
Akira Kanno <teinen.github@gmail.com>
2018-10-31 22:20:52 +0900

    Add profile content (WIP)

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,16 @@
     <div class="card">
       <h1>About</h1>
       <div class="main">
-        <img class="profile" src="@/assets/profile.png" />
+        <div class="profile-image">
+          <img class="icon" src="@/assets/profile.png" />
+        </div>
+        <div class="profile-info">
+          <h2>名前</h2>
+          <h2>年齢</h2>
+          <h2>職業</h2>
+          <h2>居住</h2>
+          <h2>趣味</h2>
+        </div>
       </div>
     </div>
   </section>
@@ -18,9 +27,20 @@ export default {
 <style lang="scss" scoped>
 .main {
   display: flex;
+  height: 90%;
+  padding-top: 20px;
 }
 
-.profile {
+.profile-image {
+  text-align: center;
+  width: 50%;
+}
+
+.profile-info {
+  width: 50%;
+}
+
+.icon {
   border: 6px solid #516C8D;
   border-radius: 50%;
   width: 300px;
